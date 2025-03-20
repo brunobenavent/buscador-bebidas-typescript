a7172d2281d450bc8ce282c28d13f236264f2af5
brunitobc <70907703+brunitobc@users.noreply.github.com>
2025-03-20 07:44:55 +0100

    primer commit

---
+++ src/components/DrinkCard.tsx
@@ -0,0 +1,30 @@
+import { useAppStore } from "../stores/useAppStore"
+import type { Drink } from "../types"
+
+type DrikCardProps = {
+    drink: Drink
+}
+
+export default function DrinkCard({drink}:DrikCardProps) {
+    const selectRecipe =useAppStore((state)=>state.selectRecipe )
+
+    return (
+        <div className="border shadow-lg">
+            <div className="overflow-hidden">
+                <img
+                    src={drink.strDrinkThumb}
+                    alt={`Imagen de ${drink.strDrink}`}
+                    className="hover:scale-125 transition-transform hover:rotate-2"
+                />
+            </div>
+            <div className="p-5 ">
+                <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+                <button
+                    onClick={()=>selectRecipe(drink.idDrink)}
+                    type="button"
+                    className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
+                >Ver Receta</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/views/FavoritesPage.tsx
@@ -0,0 +1,8 @@
+
+export default function FavoritesPage() {
+  return (
+    <div>
+       Favotitos
+    </div>
+  )
+}
